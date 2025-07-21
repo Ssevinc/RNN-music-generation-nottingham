d1df13dc6c5defc353d584d51fe3a617981d1e4c
Seyyide Sevinç <seyyidesevinc11@gmail.com>
2025-07-22 02:06:11 +0300

    vocab + character id

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from one_file import load_all_tunes
+from dataset_prep import load_all_tunes
 import random
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 all_tunes = load_all_tunes()
 
 train_tunes,test_tunes = train_test_split(
     all_tunes, test_size=0.1,random_state=42)
+
+print(train_tunes[0])
+#A single string with all the the training tunes included.
+songs_joined = "\n\n".join(train_tunes)
+
+# Creates the vocablary by finding all the unique characters
+vocab = sorted(set(songs_joined))
+
+#vectorize the text based tunes
+
+char2idx = {u: i for i, u in enumerate(vocab)}
+
+idx2char = np.array(vocab)
+
+print(char2idx)
+
 
